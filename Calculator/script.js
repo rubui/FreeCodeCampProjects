390582acce0aa57f940d634c184ac6944096ae1e
rubui <32724161+rubui@users.noreply.github.com>
2020-03-07 15:07:21 -0800

    Adding more css styling and arith key functions
    Styling top of calculator and adding fuctionality for equals and other arith

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,18 +2,21 @@ var calculator_object = {
 	'display-str':"0",
 	'curr-val': 0,
 	'saved-val': 0,
+	'mem-val': 0
 };
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 	const calculator = document.querySelector(".calc");
 	const nums = document.querySelectorAll(".num");
+	const onc = document.querySelector(".on-clear");
+	const arith = document.querySelectorAll(".arith");
 	const calculator_display = document.querySelector(".calc-display");
 
 	console.log(nums);
 	console.log(calculator_display);
 
 
-
+	//Number key functionality
 	nums.forEach((btn) => {
   		btn.addEventListener("click", (event) => {
   			console.log("hi");
@@ -28,6 +31,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     		calculator_display.innerHTML = calculator_object['display-str'];
   		});
 	});
+
+	//Clear button functionality
+	onc.addEventListener("click", (event) => {
+		console.log("clear");
+		calculator_object['curr-val'] = 0;
+		calculator_object['display-str'] = "0";
+		calculator_display.innerHTML = calculator_object['display-str'];
+	
+	});
+
+
+
+
 });
 
 
